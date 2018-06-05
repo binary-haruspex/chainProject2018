33a6c0d7f11c49f186f5b16e632794d42eb62338
binary-haruspex <39931856+binary-haruspex@users.noreply.github.com>
2018-06-04 17:50:42 -0700

    Add files via upload

---
+++ batteryMonitor.js
@@ -0,0 +1,94 @@
+// Interaction with GPIO
+var Gpio = require('onoff').Gpio
+
+// Interaction with Ethereum
+var Web3 = require('web3')
+var web3 = new Web3()
+
+// connect to the local node
+web3.setProvider(new web3.providers.HttpProvider('http://localhost:8042'))
+
+// The contract that we are going to interact with
+var contractAddress = '0x7fca200c4761ad631031d449a7a40ef2d76c211d'
+
+// Define the ABI (Application Binary Interface)
+var ABI = JSON.parse('[{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"OnValueChanged","type":"event"},{"constant":false,"inputs":[{"name":"recipient","type":"address"},{"name":"value","type":"uint256"}],"name":"depositToken","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"recipient","type":"address"},{"name":"value","type":"uint256"}],"name":"withdrawToken","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"recipient","type":"address"}],"name":"getTokens","outputs":[{"name":"value","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}]')
+
+// contract object
+var contract = web3.eth.contract(ABI).at(contractAddress)
+
+// components connected to the RPi
+var greenLed = new Gpio(14, 'out')
+var redLed = new Gpio(15, 'out')
+var button = new Gpio(18, 'in', 'rising')
+
+//Coulomb Counter Pin Assignments
+var VIO = new Gpio(40, 'out')
+var INT = new Gpio(38, 'in')
+var POL = new Gpio(36, 'in')
+var GND = new Gpio(34, 'out')
+
+//Program Variables
+var isrflag
+var time
+var lasttime
+var mA
+var ah_quanta = 0.17067759
+var percent_quanta
+
+
+
+// display initial state
+showStatus()
+
+
+
+// watch event on the button
+button.watch(function (err, value) {
+ if (err) {
+ throw err
+ }
+
+showStatus()
+})
+
+
+
+// wait for an event triggered on the Smart Contract
+var onValueChanged = contract.OnValueChanged({_from: web3.eth.coinbase});
+
+onValueChanged.watch(function(error, result) {
+ if (!error) {
+ showStatus()
+ }
+})
+
+
+
+// power the LED according the value of the token
+function showStatus() {
+ 
+ // retrieve the value of the token
+ var token = contract.getTokens(web3.eth.coinbase)
+
+// display the LED according the value of the token
+ if (token > 0) {
+ // Green: you have enough token
+ redLed.writeSync(0)
+ greenLed.writeSync(1)
+ } else {
+ // Red: not enough token
+ greenLed.writeSync(0)
+ redLed.writeSync(1)
+ }
+
+}
+
+
+
+// release process
+process.on('SIGINT', function () {
+ greenLed.unexport()
+ redLed.unexport()
+ button.unexport()
+})
