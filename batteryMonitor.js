ece427da67e047f632f5518d73fb41680efba8af
binary-haruspex <gmmoore1@nps.edu>
2018-06-06 09:11:24 -0700

    Updated BatteryMonitory.js

--- batteryMonitor.js
+++ batteryMonitor.js
@@ -1,6 +1,7 @@
 // Interaction with GPIO
 var Gpio = require('onoff').Gpio
 
+/*
 // Interaction with Ethereum
 var Web3 = require('web3')
 var web3 = new Web3()
@@ -21,7 +22,7 @@ var contract = web3.eth.contract(ABI).at(contractAddress)
 var greenLed = new Gpio(14, 'out')
 var redLed = new Gpio(15, 'out')
 var button = new Gpio(18, 'in', 'rising')
-
+*/
 //Coulomb Counter Pin Assignments
 var VIO = new Gpio(40, 'out')
 var INT = new Gpio(38, 'in')
@@ -30,14 +31,51 @@ var GND = new Gpio(34, 'out')
 
 //Program Variables
 var isrflag
-var time
-var lasttime
+var timeInt 
+var lastIntTime 
+var battery_mAh = 400.0
+var battery_percent = 100.0
 var mA
 var ah_quanta = 0.17067759
-var percent_quanta
+var percent_quanta = 1.0/(battery_mAh/1000.0*5859.0/100.0)
 
+while (True){
 
+	INT.watch(function (err, value){
 
+		if(err){
+			throw err
+		}
+
+		lastIntTime = timeInt	
+		timeInt = new Date()
+		mA = 614.4/((timeInt-lastIntTime)/1000000.0)
+
+		if (POL.read()){
+			if (polarity) mA = mA * -1.0
+			battery_mAh += ah_quanta
+		    	battery_percent += percent_quanta
+
+		} else {
+
+			battery_mAh -= ah_quanta
+			battery_percent -= percent_quanta
+		} 
+
+		console.log("mAh: ")
+	    	console.log(battery_mAh)
+	    	console.log(" soc: ")
+	    	console.log(battery_percent)
+	    	console.log("% time: ")
+	    	console.log((time-lasttime)/1000000.0)
+	    	console.log("s mA: ")
+	    	console.log(mA)
+}
+	
+		
+     
+
+/*
 // display initial state
 showStatus()
 
@@ -92,3 +130,4 @@ process.on('SIGINT', function () {
  redLed.unexport()
  button.unexport()
 })
+*/
